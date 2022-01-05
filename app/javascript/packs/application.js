047935a2f8957a9ac2c5feddfc6b03b6f23bae9a
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-01-05 16:12:37 -0300

    Import bootstrap files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,8 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "@fortawesome/fontawesome-free/js/all";
+import 'bootstrap/dist/js/bootstrap'
+import "bootstrap/dist/css/bootstrap"
 
 import './custom'
 
