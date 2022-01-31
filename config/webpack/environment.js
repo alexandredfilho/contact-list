701ba0ec2855d20ba0ab9019ad247d7e3da12a06
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-01-31 10:43:36 -0300

    Fix webpacker settings to handle jquery functions

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,9 +1,15 @@
 const { environment } = require('@rails/webpacker')
-const webpack = require("webpack")
-environment.plugins.append("Provide", new webpack.ProvidePlugin({
-    $: 'jquery',
-    jQuery: 'jquery',
-    'window.jQuery': 'jquery',
-    Popper: ['popper.js', 'default']
-  }))
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+    'Provide',
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jQuery: 'jquery',
+      jquery: 'jquery',
+      'window.jQuery': 'jquery',
+      Popper: ['popper.js', 'default'],
+    })
+)
+
 module.exports = environment
