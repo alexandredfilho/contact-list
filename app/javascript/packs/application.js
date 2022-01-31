f5b197d1b6581f3acff56ec3f38d647d80292d66
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-01-31 10:47:32 -0300

    Fix importing javascript files in main file

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,44 +1,24 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "@fortawesome/fontawesome-free/js/all";
-import 'bootstrap/dist/js/bootstrap'
-import 'bootstrap/dist/js/bootstrap.bundle.min'
-import 'bootstrap/dist/css/bootstrap.min'
-import 'bootstrap'
-import './custom'
-import '../stylesheets/application.scss'
-import 'admin-lte/dist/js/adminlte.min'
-import 'popper.js/dist/popper.min'
-import 'jquery/dist/jquery.min'
-import 'admin-lte/dist/js/adminlte'
-import 'admin-lte/dist/js/demo'
 
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 
-window.addEventListener('DOMContentLoaded', event => {
+require("bootstrap")
+import "../stylesheets/application";
+document.addEventListener("turbolinks:load", function() {
+    $(function () {
+        $('.alert').alert()
+        $('.collapse').collapse()
+        $('.dropdown-toggle').dropdown()
+        $('[data-toggle="tooltip"]').tooltip()
+        $('[data-toggle="popover"]').popover()
+    })
+})
 
-  // Toggle the side navigation
-  const sidebarToggle = document.body.querySelector('#sidebarToggle');
-  if (sidebarToggle) {
-      // Uncomment Below to persist sidebar toggle between refreshes
-      // if (localStorage.getItem('sb|sidebar-toggle') === 'true') {
-      //     document.body.classList.toggle('sb-sidenav-toggled');
-      // }
-      sidebarToggle.addEventListener('click', event => {
-          event.preventDefault();
-          document.body.classList.toggle('sb-sidenav-toggled');
-          localStorage.setItem('sb|sidebar-toggle', document.body.classList.contains('sb-sidenav-toggled'));
-      });
-  }
-
-});
+require('admin-lte');
+import "@fortawesome/fontawesome-free/js/all";
