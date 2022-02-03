a706446b6f70738286ef33c371322b7cfc3f8668
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-02-03 09:43:04 -0300

    Create a method that allows you to upload a photo to the user's model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,31 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_one_attached :photo
+  after_commit :add_default_photo, on: %i[create update]
+
+  def photo_thumbnail
+    if photo.attached?
+      photo.variant(resize: '90x90!').processed
+    else
+      "avatar.jpg"
+    end
+  end
+
+  private
+
+  def add_default_photo
+    unless photo.attached?
+      photo.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'avatar.jpg'
+          )
+        ),
+        filename: 'avatar.jpg',
+        content_type: 'image/jpg'
+      )
+    end
+  end
 end
