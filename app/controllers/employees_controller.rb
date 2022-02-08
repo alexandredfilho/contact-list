68ed0ed8239c0c273486e112217e962b55c69db4
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-02-08 18:07:52 -0300

    Change default messages

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -24,14 +24,13 @@ class EmployeesController < ApplicationController
   # POST /employees or /employees.json
   def create
     @employee = Employee.new(employee_params)
-
     respond_to do |format|
       if @employee.save
-        format.html { redirect_to employee_url(@employee), notice: "Employee was successfully created." }
-        format.json { render :show, status: :created, location: @employee }
+         format.html { redirect_to employee_url(@employee), notice: "Employee was successfully created!" }
+         format.json { render :show, status: :created, location: @employee }
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @employee.errors, status: :unprocessable_entity }
+         format.html { render :new, status: :unprocessable_entity }
+         format.json { render json: @employee.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -40,7 +39,7 @@ class EmployeesController < ApplicationController
   def update
     respond_to do |format|
       if @employee.update(employee_params)
-        format.html { redirect_to employee_url(@employee), notice: "Employee was successfully updated." }
+        format.html { redirect_to employee_url(@employee), notice: "Employee was successfully updated!" }
         format.json { render :show, status: :ok, location: @employee }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -54,7 +53,7 @@ class EmployeesController < ApplicationController
     @employee.destroy
 
     respond_to do |format|
-      format.html { redirect_to employees_url, notice: "Employee was successfully destroyed." }
+      format.html { redirect_to employees_url, notice: "Employee was successfully destroyed!" }
       format.json { head :no_content }
     end
   end
--- app/controllers/floors_controller.rb
+++ app/controllers/floors_controller.rb
@@ -25,7 +25,7 @@ class FloorsController < ApplicationController
 
     respond_to do |format|
       if @floor.save
-        format.html { redirect_to floor_url(@floor), notice: "Floor was successfully created." }
+        format.html { redirect_to floor_url(@floor), notice: "Floor was successfully created!" }
         format.json { render :show, status: :created, location: @floor }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -38,7 +38,7 @@ class FloorsController < ApplicationController
   def update
     respond_to do |format|
       if @floor.update(floor_params)
-        format.html { redirect_to floor_url(@floor), notice: "Floor was successfully updated." }
+        format.html { redirect_to floor_url(@floor), notice: "Floor was successfully updated!" }
         format.json { render :show, status: :ok, location: @floor }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -52,7 +52,7 @@ class FloorsController < ApplicationController
     @floor.destroy
 
     respond_to do |format|
-      format.html { redirect_to floors_url, notice: "Floor was successfully destroyed." }
+      format.html { redirect_to floors_url, notice: "Floor was successfully destroyed!" }
       format.json { head :no_content }
     end
   end
