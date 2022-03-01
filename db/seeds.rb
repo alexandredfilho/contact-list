a314e6d5bdc4311c30e1b5b48596726841eaed63
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-02-28 23:27:29 -0300

    Add a new employee in the seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,16 +28,30 @@ Employee.create!([
     first_name: "Alexandre",
     last_name: "Domingos",
     company: "Alex Inc.",
+    title: "Ruby on Rails Developer",
     email: "alex@test.com",
-    phone: "12345678",
+    phone: "123456789",
     department_id: 1,
+    roles: "Manager"
   },
   {
-    first_name: "Joãozin",
-    last_name: "Da Silva",
-    company: "XPTO Inc.",
-    email: "joanzin@test.com",
+    first_name: "John",
+    last_name: "Doe",
+    company: "JD Corp.",
+    title: "React Js Developer",
+    email: "john.doe@jdcorp.com",
     phone: "87654321",
     department_id: 2,
+    roles: "Employee"
+  },
+  {
+    first_name: "Stwart",
+    last_name: "Minion",
+    company: "Minons Inc.",
+    email: "stwart@minions.com",
+    phone: "12345689",
+    title: "CIO",
+    department_id: 1,
+    roles: "Manager"
   }
 ])
