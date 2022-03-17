30dd07c9f2c840ebceed8b369ee6845ba4c2e719
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-03-16 22:17:25 -0300

    Add method that sets a default photo for employees and removes unused enums scopes, complement the thumbnail method and add a new method that checks whether or not the employee has a manager

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -3,23 +3,48 @@ class Employee < ApplicationRecord
   has_one_attached :photo
   has_many :subordinates, class_name: "Employee", foreign_key: "manager_id"
   belongs_to :manager, class_name: "Employee", optional: true
+  after_commit :default_photo, on: %i[create update]
 
-  def thumbnail
-    return self.photo.variant(resize: '90x90!').processed
+  def photo_thumbnail
+    photo = self.photo
+    if photo.attached?
+      photo.variant(resize: "90x90!").processed
+    else
+      image_tag ("avatar.jpg"), size:"90"
+    end
+  end
+
+  def manager_photo
+    manager = self.manager
+    if manager.present? && manager.photo.attached?
+      manager.photo.variant(resize: '90x90!').processed
+    else
+      "avatar.jpg"
+    end
   end
 
   enum roles: {
-    Employee: 0,
-    Coordinator: 1,
-    Supervisor: 2,
-    Manager: 3
+    employee: 0,
+    coordinator: 1,
+    supervisor: 2,
+    manager: 3
   }
-
-  scope :employee,    -> { where(roles: 'Employee') }
-  scope :coordinator, -> { where(roles: 'Coordinator') }
-  scope :supervisor,  -> { where(roles: 'Supervisor') }
-  scope :manager,     -> { where(roles: 'Manager') }
 
   paginates_per 3
 
+  private
+
+  def default_photo
+    unless self.photo.attached?
+      self.photo.attach(
+        io: File.open(
+          Rails.root.join(
+            'app', 'assets', 'images', 'avatar.jpg'
+          )
+        ),
+        filename: 'avatar.jpg',
+        content_type: 'image/jpg'
+      )
+    end
+  end
 end
