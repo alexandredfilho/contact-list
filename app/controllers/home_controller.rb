320a4985f0697cc1a1d6d12a5e8292ed42d711ba
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-04-04 21:56:06 -0300

    Create an instance variable to store the contents of upcoming birthdays

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,8 @@ class HomeController < ApplicationController
     @employees = Employee.all
     @departments = Department.all
     @floors = Floor.all
+
+    @todays_birthdays = Employee.todays_birthdays.sort
+    @upcoming_birthdays = Employee.upcoming_birthdays
   end
 end
