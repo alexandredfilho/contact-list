9bf46bc5ce938f0f32201216be61c84c40a842a9
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-04-21 22:35:50 -0300

    Remove mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -121,16 +121,4 @@ Rails.application.configure do
   #Email
   config.action_mailer.default_url_options = { host: 'contact-list-v1.herokuapp.com' }
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:              'smtp.gmail.com',
-    port:                 587,
-    domain:               'gmail.com',
-    user_name:            Rails.application.credentials.gmail[:user_name].to_s,
-    password:             Rails.application.credentials.gmail[:user_name],
-    authentication:       'plain',
-    enable_starttls_auto: true,
-    open_timeout:         5,
-    read_timeout:         5 }
-
 end
