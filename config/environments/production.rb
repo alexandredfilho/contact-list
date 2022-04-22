3954d31a78b3f8b159e8ef52ed1284ffe32cd03d
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-04-21 22:31:00 -0300

    Fix heroku connection

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,7 +119,7 @@ Rails.application.configure do
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
 
   #Email
-  config.action_mailer.default_url_options = { host: 'contact-list-beta.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'contact-list-v1.herokuapp.com' }
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
