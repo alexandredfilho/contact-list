8995fd2ae1b5539289c317901d4262bdade748d2
Alexandre Domingos Filho <alexandre.filho@adfsolucoesemti.com.br>
2022-05-09 11:45:52 -0300

    Add new settings to handle jQuery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,7 +2,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "jquery/dist/jquery"
+require("jquery/src/jquery")
 require("bootstrap")
 require('admin-lte');
 import "popper.js/dist/popper"
@@ -10,6 +10,8 @@ import "../stylesheets/application";
 import "@fortawesome/fontawesome-free/js/all";
 global.toastr = require("toastr")
 
+window.jQuery = $;
+window.$ = $;
 
 Rails.start()
 Turbolinks.start()
@@ -22,5 +24,6 @@ document.addEventListener("turbolinks:load", function() {
         $('.dropdown-toggle').dropdown()
         $('[data-toggle="tooltip"]').tooltip()
         $('[data-toggle="popover"]').popover()
+        $('[data-toggle=dropdown]').dropdown()
     })
 })
